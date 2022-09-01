d016402db6f50e7a7d3976d6f64ab7aa5cae77a3
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-09-01 16:59:04 +0200

    redirected the login to the profile page and created a header component to display important information on profile page

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import _ from 'lodash';
+import { createBrowserHistory } from 'history';
+import { Route, Switch, Router } from 'react-router-dom';
 import Login from '../components/Login';
 import Register from '../components/Register';
 import Profile from '../components/Profile';
+import Header from '../components/Header';
 
-const AppRouter = () => {
+export const history = createBrowserHistory();
+
+const AppRouter = ({ auth }) => {
     return (
-        <BrowserRouter>
-            <div className="container">
-                <Switch>
-                    <Route path="/" component={Login} exact={true} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/profile" component={Profile} />
-                </Switch>
+        <Router history={history}>
+            <div>
+                {!_.isEmpty(auth.token) && <Header />}
+                <div className="container">
+                    <Switch>
+                        <Route path="/" component={Login} exact={true} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/profile" component={Profile} />
+                    </Switch>
+                </div>
             </div>
-        </BrowserRouter>
+        </Router>
     );
 };
 
