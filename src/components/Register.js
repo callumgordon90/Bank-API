bc7450972fbf42e7458c4d2ad7d6c353ab699313
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-09-01 16:38:00 +0200

    all profile components added in the src directory

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { Form, Button } from 'react-bootstrap';
 import { registerNewUser } from '../actions/auth';
+import { resetErrors } from '../actions/errors';
 import { validateFields } from '../utils/common';
 import { Link } from 'react-router-dom';
 
@@ -16,6 +18,16 @@ class Register extends React.Component {
         errorMsg: '',
         isSubmitted: false
     };
+
+    componentDidUpdate(prevProps) {
+        if (!_.isEqual(prevProps.errors, this.props.errors)) {
+            this.setState({ errorMsg: this.props.errors });
+        }
+    }
+
+    componentWillUnmount() {
+        this.props.dispatch(resetErrors());
+    }
 
     registerUser = (event) => {
         event.preventDefault();
@@ -142,4 +154,8 @@ class Register extends React.Component {
     }
 }
 
-export default connect()(Register);
+const mapStateToProps = (state) => ({
+    errors: state.errors
+});
+
+export default connect(mapStateToProps)(Register);
